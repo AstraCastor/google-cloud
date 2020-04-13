ba19d0603b2ce131a5fe1306197f802f258a2815
Praveen Rajkumar <prrajkumar@google.com>
2020-04-13 09:02:40 -0700

    Iso mode, added metadata table

--- talent_solution/src/db.py
+++ talent_solution/src/db.py
@@ -19,41 +19,42 @@ class cts_db():
 
     def __init__(self):
         try:
-            __connection = sqlite3.connect("./res/cts.db")
+            __connection = sqlite3.connect("./res/cts.db",isolation_level=None)
             self.__cursor = __connection.cursor()
-            logger.info("DB Connection created.")
+            logger.info("CTS Database connected.")
             self.setup_cts_schema(self.__cursor)
         except ConnectionError as e:
             logger.exception("Error when opening DB connection. Message:{}".format(e))
         finally:
             __connection.close
 
+    def connection(self):
+        return self.__cursor
 
     def setup_cts_schema(self,cursor):
         try:
             sql_create_table = {}
+            sql_create_table['metadata'] = "CREATE TABLE IF NOT EXISTS metadata (   \
+                                            table_nm TEXT PRIMARY KEY, \
+                                            sync_time INTEGER)"
+
             sql_create_table['tenant'] = "CREATE TABLE IF NOT EXISTS tenant (  \
                                         external_id TEXT PRIMARY KEY,   \
-                                        tenant_name TEXT,   \
+                                        tenant_name TEXT NOT NULL,   \
+                                        project_id TEXT NOT NULL,    \
                                         suspended INTEGER,  \
                                         create_time INTEGER)"
 
             sql_create_table['company'] = "CREATE TABLE IF NOT EXISTS company (  \
                                         external_id TEXT PRIMARY KEY,   \
                                         company_name TEXT,   \
+                                        project_id TEXT NOT NULL,    \
                                         suspended INTEGER,  \
                                         create_time INTEGER)"
 
             for table in sql_create_table:
                 cursor.execute(sql_create_table[table])
-                logger.info("Table {} created.".format(table))
+                logger.info("Table {} is available.".format(table))
         except Exception as e:
             logger.exception("Error when creating table {}. Message: {}".format(table,e))
-
-
-    def connection(self):
-        return self.__cursor
-
-
-
 
