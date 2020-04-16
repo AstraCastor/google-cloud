3ba3e9e37de927faf0d662755ad4f1f31f53659a
Praveen Rajkumar <prrajkumar@google.com>
2020-04-15 17:12:03 -0700

    Minor fixes and added debug messages

--- talent_solution/modules/cts_company.py
+++ talent_solution/modules/cts_company.py
@@ -1,5 +1,5 @@
 from google.cloud import talent_v4beta1,storage
-import os,sys,logging,argparse
+import os,sys,logging,argparse, inspect
 from datetime import datetime
 from . import cts_db,cts_tenant
 
@@ -25,10 +25,12 @@ class Company:
             logger.debug("Company client created: {}".format(self._company_client))
             self._db_connection = cts_db.DB().connection()
             logger.debug("Company db connection obtained: {}".format(self._db_connection))
+            logging.debug("Company instantiated.")
         except Exception as e:
             logging.exception("Error instantiating Company. Message: {}".format(e))
 
     def create_company(self,project_id,tenant_id=None,company_object=None,file=None):
+        logger.debug("CALLED: create_company({},{},{},{} by {})".format(project_id,tenant_id,company_object,file,inspect.currentframe().f_back.f_code.co_name,))
         db = self._db_connection
         client = self._company_client
         try:
@@ -36,7 +38,8 @@ class Company:
             if isinstance(company_object,dict):
                 external_id = company_object['external_id']
                 # Check if it is an existing company
-                existing_company = self.get_company(project_id=project_id,external_id=external_id)
+                logger.debug("{}:Calling get_company({},{})".format(inspect.currentframe().f_code.co_name,project_id,external_id))
+                existing_company = self.get_company(project_id=project_id,tenant_id=tenant_id,external_id=external_id)
                 if existing_company is None:
                     if tenant_id is not None:
                         # To set the parent of the company to be created to the tenant_name 
@@ -48,26 +51,30 @@ class Company:
                             logging.error("Unknown Tenant: {}".format(tenant_id))
                             exit(1)
                         parent = tenant_obj.name
+                        tenant_name = tenant_obj.name
                     else:
+                        tenant_name = None
+                        tenant_id = ""
                         parent = client.project_path(project_id)
-                    logger.debug("Parent path set to: {}".format(parent))
+                    logger.debug("{}:Parent path set to: {}".format(inspect.currentframe().f_code.co_name,parent))
                     new_company = client.create_company(parent,company_object)
                     query = "INSERT INTO company (company_key,external_id,company_name,tenant_name,project_id,suspended,create_time)    \
-                        VALUES ('{}','{}','{}','{}','{}','{:d}','{}')".format(project_id+"-"+external_id,new_company.external_id,    \
-                            new_company.name,tenant_obj.name,project_id,0,datetime.now())
+                        VALUES ('{}','{}','{}','{}','{}','{:d}','{}')".format(project_id+"-"+tenant_id+"-"+external_id,new_company.external_id,    \
+                            new_company.name,tenant_name,project_id,0,datetime.now())
                     logger.debug("QUERY: {}".format(query))
                     db.execute(query)
                     logger.info("Company {} created.\n{}".format(external_id,new_company))
+                    print("Company {} created.\n{}".format(external_id,new_company))
                     return new_company
                 else:
-                    logger.error("Company {} already exists.\n{}".format(external_id,existing_company))
+                    logger.error("{}: Company {} already exists.\n{}".format(inspect.currentframe().f_code.co_name,external_id,existing_company))
                     return None
             else:
-                logger.error("Invalid or missing company argument. Should be a valid company object.\n {}"\
-                    .format(company_object))
+                logger.error("{}:Invalid or missing company argument. Should be a valid company object.\n {}"\
+                    .format(inspect.currentframe().f_code.co_name,company_object))
                 raise ValueError
         except Exception as e:
-            print("Error creating company:\n{}\n{}".format(company_object,e))
+            print("{}:Error creating company:\n{}\n{}".format(inspect.currentframe().f_code.co_name,company_object,e))
             self.delete_company(project_id,tenant_id,company_object['external_id'],forced=True)
             raise
 
@@ -79,6 +86,7 @@ class Company:
         Returns:
             None - If company is not found.
         """
+        logger.debug("CALLED: delete_company({},{},{},{},{} by {})".format(project_id,tenant_id,external_id,all,forced,inspect.currentframe().f_back.f_code.co_name))
         db = self._db_connection
         client = self._company_client
         try:
@@ -95,18 +103,22 @@ class Company:
                 else:
                     existing_company = None
             else:
+                logger.debug("{}:Calling get_company({},{},{})".format(inspect.currentframe().f_code.co_name,project_id,tenant_id,external_id))
                 existing_company = self.get_company(project_id=project_id,tenant_id=tenant_id,external_id=external_id)
-                logger.debug("Existing company? {}".format(existing_company))
+                logger.debug("{}:Existing company? {}".format(inspect.currentframe().f_code.co_name,existing_company))
             if existing_company is not None:
                 logger.info("Deleting company id: {}".format(existing_company.external_id))
                 client.delete_company(existing_company.name)
                 db.execute("DELETE FROM company where company_name = '{}'".format(existing_company.name))
                 logger.info("Company {} deleted.".format(external_id))
+                print("Company {} deleted.".format(external_id))
+
             else:
-                logger.error("Company {} does not exist.".format(external_id))
+                logger.error("{}: Company {} does not exist.".format(inspect.currentframe().f_code.co_name,external_id))
+                print("Company {} does not exist.".format(external_id))
                 return None
         except Exception as e:
-            print("Error deleting company {}: {}".format(external_id,e))
+            print("{}:Error deleting company {}: {}".format(inspect.currentframe().f_code.co_name,external_id,e))
             raise
 
     def update_company(self,tenant_id,project_id=None,company=None,file=None):
@@ -121,42 +133,43 @@ class Company:
         Returns:
             an instance of company or None if company was not found.
         """
-        logger.debug("Get company called.")
+        logger.debug("CALLED: get_company({},{},{},{} by {})".format(project_id,tenant_id,external_id,all,inspect.currentframe().f_back.f_code.co_name))
         db = self._db_connection
         client = self._company_client
         try:
             if external_id is not None:
                 if all:
-                    logging.exception("Conflicting arguments: --external_id and --all are mutually exclusive.")
+                    logging.exception("{}:Conflicting arguments: --external_id and --all are mutually exclusive.".format(inspect.currentframe().f_code.co_name))
                     raise ValueError
                 company_key = project_id+"-"+tenant_id+"-"+external_id if project_id is not None and tenant_id is not None \
                 else project_id+"-"+external_id
+                logger.debug("{}:Searching for company: {}".format(inspect.currentframe().f_code.co_name,company_key))
                 db.execute("SELECT company_name FROM company where company_key = '{}'".format(company_key))
                 rows = db.fetchall()
                 if rows == []:
                     return None
                 else:
                     logger.debug("db lookup:{}".format(rows))
-                    company = client.get_company(rows[0][1])
+                    company = client.get_company(rows[0][0])
             elif all:
                 if tenant_id is not None:
                     tenant = cts_tenant.Tenant()
                     tenant_obj = tenant.get_tenant(project_id,tenant_id)
-                    logger.debug("Tenant retrieved:\n{}".format(tenant_obj))
+                    logger.debug("{}:Tenant retrieved:\n{}".format(inspect.currentframe().f_code.co_name,tenant_obj))
                     if tenant_obj is None:
-                        logging.error("Unknown Tenant: {}".format(tenant_id))
+                        logging.error("{}:Unknown Tenant: {}".format(inspect.currentframe().f_code.co_name,tenant_id))
                         exit(1)
                     parent = tenant_obj.name
                 else:
                     parent = client.project_path(project_id)
-                logger.debug("Parent path: {}".format(parent))
+                logger.debug("{}:Parent path: {}".format(inspect.currentframe().f_code.co_name,parent))
                 company = [t for t in client.list_companies(parent)]
-                return company
             else:
                 logger.exception("Invalid arguments.")
                 raise AttributeError
+            return company
         except Exception as e:
-            logger.error("Error getting company by name {}. Message: {}".format(external_id,e))
+            logger.error("{}:Error getting company by name {}. Message: {}".format(inspect.currentframe().f_code.co_name,external_id,e))
             raise 
 
 if __name__ == '__main__':
