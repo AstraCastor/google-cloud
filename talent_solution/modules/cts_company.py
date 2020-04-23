22493bd3662c2847d1a2919b9935a4ec1bd7ceba
Praveen Rajkumar <prrajkumar@google.com>
2020-04-22 23:53:54 -0700

    Multi company show & a TODO of better err handling

--- talent_solution/modules/cts_company.py
+++ talent_solution/modules/cts_company.py
@@ -1,5 +1,7 @@
 from google.cloud import talent_v4beta1
-import os,sys,logging,argparse, inspect
+from google.api_core.exceptions import AlreadyExists, NotFound, GoogleAPICallError, RetryError
+
+import os,sys,logging,argparse,inspect,re
 from datetime import datetime
 from modules import cts_db,cts_tenant
 
@@ -42,6 +44,7 @@ class Company:
                 # Check if it is an existing company
                 logger.debug("{}:Calling get_company({},{})".format(inspect.currentframe().f_code.co_name,project_id,external_id))
                 existing_company = self.get_company(project_id=project_id,tenant_id=tenant_id,external_id=external_id)
+                logger.debug("{}:get_company returned:{}".format(inspect.currentframe().f_code.co_name,existing_company))
                 if existing_company is None:
                     if tenant_id is not None:
                         # To set the parent of the company to be created to the tenant_name 
@@ -56,12 +59,46 @@ class Company:
                         tenant_name = tenant_obj.name
                     else:
                         tenant_name = None
-                        tenant_id = ""
+                        # tenant_id = ""
                         parent = client.project_path(project_id)
                     logger.debug("{}:Parent path set to: {}".format(inspect.currentframe().f_code.co_name,parent))
-                    new_company = client.create_company(parent,company_object)
+                    try:
+                        new_company ={}
+                        new_company = client.create_company(parent,company_object)
+                    except AlreadyExists as e:                    
+                        logger.debug("Company {} exists in server. (TODO)Creating local record..".format(company_object))
+                        #TODO: Better error handling
+                        # print("Error Message:{}".format(e.message))
+                        # company_name_re = re.compile('^Company (.*?) (:?.*)')
+                        # regx_search = company_name_re.findall(e.message)
+                        # print("REGEX OUT {}".format(regx_search))
+                        # print ("REGEX TYPE: {}".format(type(regx_search)))
+                        # company_name = regx_search
+                        # print("REGEX[0][0]".format(str(regx_search[0][0])))
+                        # print ("Company Name:".format(regx_search[0][0]))
+                        # print ("Company Name:".format(regx_search[0][1]))
+                        # print ("Company Name:".format(regx_search[0]))
+
+                        # print ("Company Name : {}".format(company_name))
+                        # print ("Company Name : {}".format(company_name[0]))
+                        # print ("Company Name : {}".format(company_name[0][0]))
+                        # print ("Company Name : {}".format(company_name[0][1]))
+
+                        # new_company['external_id']=company_object['external_id']
+                        # new_company['company_name'] = company_name[0][0]
+                        # print ("Type of New company {}".format(type(new_company)))
+                        # print ("New company length {}".format(len(new_company)))
+                        # for k,v in new_company.items():
+                        #        print ("New Company items: {} {}".format(k,v)) 
+                        # print("New company after error:".format(new_company))
+                        pass
+                    except Exception as e:
+                        raise e
+
+                    company_key = project_id+"-"+tenant_id+"-"+external_id if tenant_id is not None else project_id+"-"+external_id
+                    logger.debug("Inserting record for company key:{}".format(company_key))
                     query = "INSERT INTO company (company_key,external_id,company_name,tenant_name,project_id,suspended,create_time)    \
-                        VALUES ('{}','{}','{}','{}','{}','{:d}','{}')".format(project_id+"-"+tenant_id+"-"+external_id,new_company.external_id,    \
+                        VALUES ('{}','{}','{}','{}','{}','{:d}','{}')".format(company_key,new_company.external_id,    \
                             new_company.name,tenant_name,project_id,0,datetime.now())
                     logger.debug("QUERY: {}".format(query))
                     db.execute(query)
@@ -93,6 +130,7 @@ class Company:
         client = self._company_client
         try:
             if forced:
+                # Get all the companies from the server directly - get_company(all,scope=full) gets everything from the server directly
                 all_companies = self.get_company(project_id=project_id,tenant_id=tenant_id,all=True)
                 logger.debug("Total companies retrieved: {}".format(len(all_companies)))
                 if len(all_companies) != 0:
@@ -109,9 +147,9 @@ class Company:
                 existing_company = self.get_company(project_id=project_id,tenant_id=tenant_id,external_id=external_id)
                 logger.debug("{}:Existing company? {}".format(inspect.currentframe().f_code.co_name,existing_company))
             if existing_company is not None:
-                logger.info("Deleting company id: {}".format(existing_company.external_id))
-                client.delete_company(existing_company.name)
-                db.execute("DELETE FROM company where company_name = '{}'".format(existing_company.name))
+                logger.info("Deleting company id: {}".format(existing_company[0].external_id))
+                client.delete_company(existing_company[0].name)
+                db.execute("DELETE FROM company where company_name = '{}'".format(existing_company[0].name))
                 logger.info("Company {} deleted.".format(external_id))
                 print("Company {} deleted.".format(external_id))
 
@@ -126,7 +164,7 @@ class Company:
     def update_company(self,tenant_id,project_id=None,company=None,file=None):
         pass
 
-    def get_company(self,project_id,tenant_id=None,external_id=None,all=False):
+    def get_company(self,project_id,tenant_id=None,external_id=None,all=False,scope='full'):
         """ Get CTS company by name or get all CTS companies by project.
         Args:
             project_id: Project where the company will be created - string
@@ -135,7 +173,7 @@ class Company:
         Returns:
             an instance of company or None if company was not found.
         """
-        logger.debug("CALLED: get_company({},{},{},{} by {})".format(project_id,tenant_id,external_id,all,inspect.currentframe().f_back.f_code.co_name))
+        logger.debug("CALLED: get_company({},{},{},{} by function {})".format(project_id,tenant_id,external_id,all,inspect.currentframe().f_back.f_code.co_name))
         db = self._db_connection
         client = self._company_client
         try:
@@ -143,23 +181,35 @@ class Company:
                 if all:
                     logging.exception("{}:Conflicting arguments: --external_id and --all are mutually exclusive.".format(inspect.currentframe().f_code.co_name))
                     raise ValueError
-                company_key = project_id+"-"+tenant_id+"-"+external_id if project_id is not None and tenant_id is not None \
-                else project_id+"-"+external_id
-                logger.debug("{}:Searching for company: {}".format(inspect.currentframe().f_code.co_name,company_key))
-                db.execute("SELECT company_name FROM company where company_key = '{}'".format(company_key))
+                # company_key = project_id+"-"+tenant_id+"-"+external_id if project_id is not None and tenant_id is not None \
+                # else project_id+"-"+external_id
+                company_keys=[project_id+"-"+tenant_id+"-"+company_id if project_id is not None and tenant_id is not None \
+                else project_id+"-"+company_id for company_id in external_id.split(",")]
+                # logger.debug("{}:Searching for company: {}".format(inspect.currentframe().f_code.co_name,company_key))
+                logger.debug("{}:Searching for company: {}".format(inspect.currentframe().f_code.co_name,company_keys))
+                # db.execute("SELECT company_name FROM company where company_key = '{}'".format(company_key))
+                db.execute("SELECT external_id,company_name FROM company where company_key in ({})"\
+                    .format(",".join("?"*len(company_keys))),company_keys)
                 rows = db.fetchall()
                 if rows == []:
                     return None
                 else:
                     logger.debug("db lookup:{}".format(rows))
-                    company = client.get_company(rows[0][0])
+                    # company = client.get_company(rows[0][0])
+                    if scope == 'limited':
+                        company = [object('{"external_id":"'+row[0]+'","name":"'+row[1]+'"}"') for row in rows]
+#                        company = [namedtuple("Company",company_list_item.keys())(*company_list_item.values()) \
+#                            for company_list_item in company_list]
+                        print ("Company : {}".format(company))
+                    else:
+                        company = [client.get_company(row[1]) for row in rows]
             elif all:
                 if tenant_id is not None:
                     tenant = cts_tenant.Tenant()
                     tenant_obj = tenant.get_tenant(project_id,tenant_id)
                     logger.debug("{}:Tenant retrieved:\n{}".format(inspect.currentframe().f_code.co_name,tenant_obj))
                     if tenant_obj is None:
-                        logging.error("{}:Unknown Tenant: {}".format(inspect.currentframe().f_code.co_name,tenant_id))
+                        logger.error("{}:Unknown Tenant: {}".format(inspect.currentframe().f_code.co_name,tenant_id))
                         exit(1)
                     parent = tenant_obj.name
                 else:
