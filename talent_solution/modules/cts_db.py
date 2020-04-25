189ade8b6c37cc72a2893b7c8ca68d53fa865117
Praveen Rajkumar <prrajkumar@google.com>
2020-04-24 20:33:17 -0700

    modified logger, refactored class to check schema

--- talent_solution/modules/cts_db.py
+++ talent_solution/modules/cts_db.py
@@ -2,76 +2,106 @@ import sqlite3
 import logging
 import sys
 import os
+from res import config as config
 
 #General logging config
-log_level = os.environ.get('LOG_LEVEL','INFO')
-logger = logging.getLogger(__name__)
-logger.setLevel(log_level)
-logger_format = logging.Formatter('%(asctime)s %(filename)s:%(lineno)s %(levelname)-8s %(message)s','%Y-%m-%d %H:%M',)
+logger = logging.getLogger()
 
-#Console Logging
-console_logger = logging.StreamHandler()
-console_logger.setLevel(log_level)
-console_logger.setFormatter(logger_format)
-logger.addHandler(console_logger)
 
 class DB():
 
     def __init__(self):
         try:
-            __connection = sqlite3.connect("./res/cts.db",isolation_level=None)
+            # __connection = sqlite3.connect("./res/cts.db",isolation_level=None)
+            __connection = sqlite3.connect(config.DATABASE['file'],isolation_level=None)
             self.__cursor = __connection.cursor()
             logger.debug("CTS Database connected.")
-            self.setup_cts_schema(self.__cursor)
+            self.check_cts_schema(self.__cursor)
         except ConnectionError as e:
             logger.exception("Error when opening DB connection. Message:{}".format(e))
-        finally:
-            __connection.close
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+        
+    @property
     def connection(self):
         return self.__cursor
 
-    def setup_cts_schema(self,cursor):
+    def cts_schema(self):
+        sql_cts_table = {}
+
+        sql_cts_table['metadata'] = "CREATE TABLE IF NOT EXISTS metadata (   \
+                                        table_nm TEXT PRIMARY KEY, \
+                                        sync_time INTEGER)"
+
+        sql_cts_table['tenant'] = "CREATE TABLE IF NOT EXISTS tenant (  \
+                                    tenant_key TEXT PRIMARY KEY,     \
+                                    tenant_name TEXT NOT NULL,   \
+                                    external_id TEXT,   \
+                                    project_id TEXT NOT NULL,    \
+                                    suspended INTEGER,  \
+                                    create_time INTEGER \
+                                    )"
+
+        sql_cts_table['company'] = "CREATE TABLE IF NOT EXISTS company (  \
+                                    company_key TEXT PRIMARY KEY, \
+                                    external_id TEXT NOT NULL,   \
+                                    company_name TEXT,   \
+                                    tenant_name TEXT, \
+                                    project_id TEXT NOT NULL,    \
+                                    suspended INTEGER,  \
+                                    create_time INTEGER    \
+                                    )"
+
+        sql_cts_table['job'] = "CREATE TABLE IF NOT EXISTS job (  \
+                                    job_key TEXT PRIMARY KEY, \
+                                    external_id TEXT NOT NULL,   \
+                                    language_code TEXT NOT NULL, \
+                                    job_name TEXT, \
+                                    company_name TEXT NOT NULL,   \
+                                    tenant_name TEXT, \
+                                    project_id TEXT NOT NULL,    \
+                                    suspended INTEGER,  \
+                                    create_time INTEGER    \
+                                    )"
+        return sql_cts_table
+
+
+    def check_cts_schema(self,cursor):
         try:
-            sql_create_table = {}
-            sql_create_table['metadata'] = "CREATE TABLE IF NOT EXISTS metadata (   \
-                                            table_nm TEXT PRIMARY KEY, \
-                                            sync_time INTEGER)"
-
-            sql_create_table['tenant'] = "CREATE TABLE IF NOT EXISTS tenant (  \
-                                        tenant_key TEXT PRIMARY KEY,     \
-                                        tenant_name TEXT NOT NULL,   \
-                                        external_id TEXT,   \
-                                        project_id TEXT NOT NULL,    \
-                                        suspended INTEGER,  \
-                                        create_time INTEGER \
-                                        )"
-
-            sql_create_table['company'] = "CREATE TABLE IF NOT EXISTS company (  \
-                                        company_key TEXT PRIMARY KEY, \
-                                        external_id TEXT NOT NULL,   \
-                                        company_name TEXT,   \
-                                        tenant_name TEXT, \
-                                        project_id TEXT NOT NULL,    \
-                                        suspended INTEGER,  \
-                                        create_time INTEGER    \
-                                        )"
-
-            sql_create_table['job'] = "CREATE TABLE IF NOT EXISTS job (  \
-                                        job_key TEXT PRIMARY KEY, \
-                                        external_id TEXT NOT NULL,   \
-                                        language_code TEXT NOT NULL, \
-                                        job_name TEXT, \
-                                        company_name TEXT NOT NULL,   \
-                                        tenant_name TEXT, \
-                                        project_id TEXT NOT NULL,    \
-                                        suspended INTEGER,  \
-                                        create_time INTEGER    \
-                                        )"
-
-            for table in sql_create_table:
-                cursor.execute(sql_create_table[table])
-                logger.debug("Table {} is available.".format(table))
+            schema = self.cts_schema()
+            errors = []
+            for table in schema:
+                try:
+                    cursor.execute("select True from {}".format(table))
+                except sqlite3.OperationalError as e:
+                    logger.error(e)
+                    logger.warn("DB Check: {} is missing and will be recreated.".format(table)) 
+                    self.create_cts_table(cursor,table)
+                except Exception as e:
+                    errors.append(e)
+                else:
+                    logger.debug("DB check: {} OK".format(table))
+            if errors:
+                raise MultipleErrors(errors)
         except Exception as e:
-            logger.exception("Error when creating table {}. Message: {}".format(table,e))
+            logger.error("Schema check failed. Message: {}".format(e),exc_info=config.LOGGING['traceback'])
+
+
+    def create_cts_table(self,cursor,table=None):
+        try:
+            schema = self.cts_schema()
+            if table is None:
+                for t in schema:
+                    cursor.execute(schema[t])
+                    logger.debug("Table {} is created.".format(t))
+            else:
+                cursor.execute(schema[table])
+                logger.debug("Table {} is created.".format(table))
+
+        except Exception as e:
+            logger.error("Error when creating table {}. Message: {}".format(table,e),exc_info=config.LOGGING['traceback'])
 
